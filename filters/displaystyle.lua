01a2145107fa98eca48b157e7408c879dd379879
Gousev Ivan <vanyagosev@gmail.com>
2026-07-12 02:00:40 +0300

    cleaning, 4 and 6 lects

--- filters/displaystyle.lua
+++
@@ -1,7 +0,0 @@
-function Math(elem)
-  -- Проверяем, является ли формула инлайн и содержит ли она \displaystyle
-  if elem.mathtype == 'InlineMath' and elem.text:find('\\displaystyle') then
-    -- Превращаем её в выключную формулу (DisplayMath)
-    return pandoc.Math('DisplayMath', elem.text)
-  end
-end
